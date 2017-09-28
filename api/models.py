6cd1080293324f292a75a70a5cf5410c6222ff34
Lynn Tran <lynn.tran18@gmail.com>
2017-09-28 13:15:10 -0400

    Updated model

--- api/models.py
+++ api/models.py
@@ -20,8 +20,8 @@ class User(db.Model):
     email = db.Column(db.String(255), unique=True)
     password = db.Column(db.String(255))
     active = db.Column(db.Boolean())
-    #confirmed_at = db.Column(db.DateTime())
-    #roles = db.relationship('Role',secondary=roles_users, backref=db.backref('users', lazy='dynamic'))
+    confirmed_at = db.Column(db.DateTime())
+    roles = db.relationship('Role',secondary=roles_users, backref=db.backref('users', lazy='dynamic'))
 
     def __repr__(self):
         return '<User %r>' % (self.email)
