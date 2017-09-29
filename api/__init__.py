751e46db476a9d301ee9b63b719aca661eff2170
Lynn Tran <lynn.tran18@gmail.com>
2017-09-29 14:56:05 -0400

    Switched from sqlite to postgres uri

--- api/__init__.py
+++ api/__init__.py
@@ -3,15 +3,14 @@ import os
 from flask import Flask, jsonify
 from flask_cors import CORS
 from flask_admin import Admin
-from flask_sqlalchemy import SQLAlchemy
+from flask.ext.sqlalchemy import SQLAlchemy
 from flask_security import SQLAlchemyUserDatastore, Security
 
 app_name = 'tableflip'
 app = Flask(app_name)
 
 # NOTE: Expect to setup all these things before you can import views/models
-# config value for database
-app.config.from_object('config')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/{db}'.format(db=app_name)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # config value for Flask-Security by using PBKDF2 with salt
@@ -21,10 +20,10 @@ CORS(app)
 
 db = SQLAlchemy(app)
 roles_users = db.Table(
-    'roles_users',
-    db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
-    db.Column('role_id', db.Integer(), db.ForeignKey('role.id'))
-)
+        'roles_users',
+        db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+        db.Column('role_id', db.Integer(), db.ForeignKey('role.id'))
+        )
 
 from api import models, views
 
--- api/models.py
+++ api/models.py
@@ -19,9 +19,9 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(255), unique=True)
     password = db.Column(db.String(255))
-    active = db.Column(db.Boolean())
-    confirmed_at = db.Column(db.DateTime())
-    roles = db.relationship('Role',secondary=roles_users, backref=db.backref('users', lazy='dynamic'))
+    #active = db.Column(db.Boolean())
+    #confirmed_at = db.Column(db.DateTime())
+    #roles = db.relationship('Role',secondary=roles_users, backref=db.backref('users', lazy='dynamic'))
 
     def __repr__(self):
         return '<User %r>' % (self.email)
