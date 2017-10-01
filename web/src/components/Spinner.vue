8956d95b5e793effd47afb762db25af183b4e07e
neynt <hyriodula@gmail.com>
2017-09-30 22:34:53 -0400

    add loading spinner component

---
+++ web/src/components/Spinner.vue
@@ -0,0 +1,32 @@
+<template>
+<svg width="64px" height="64px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" style="background: none;"><g transform="rotate(0 50 50)">
+  <rect x="45" y="10" rx="4.5" ry="1" width="10" height="40" fill="#a9a9a9">
+    <animate attributeName="opacity" values="1;0" times="0;1" dur="1s" begin="-0.8333333333333334s" repeatCount="indefinite"></animate>
+  </rect>
+</g><g transform="rotate(60 50 50)">
+  <rect x="45" y="10" rx="4.5" ry="1" width="10" height="40" fill="#a9a9a9">
+    <animate attributeName="opacity" values="1;0" times="0;1" dur="1s" begin="-0.6666666666666666s" repeatCount="indefinite"></animate>
+  </rect>
+</g><g transform="rotate(120 50 50)">
+  <rect x="45" y="10" rx="4.5" ry="1" width="10" height="40" fill="#a9a9a9">
+    <animate attributeName="opacity" values="1;0" times="0;1" dur="1s" begin="-0.5s" repeatCount="indefinite"></animate>
+  </rect>
+</g><g transform="rotate(180 50 50)">
+  <rect x="45" y="10" rx="4.5" ry="1" width="10" height="40" fill="#a9a9a9">
+    <animate attributeName="opacity" values="1;0" times="0;1" dur="1s" begin="-0.3333333333333333s" repeatCount="indefinite"></animate>
+  </rect>
+</g><g transform="rotate(240 50 50)">
+  <rect x="45" y="10" rx="4.5" ry="1" width="10" height="40" fill="#a9a9a9">
+    <animate attributeName="opacity" values="1;0" times="0;1" dur="1s" begin="-0.16666666666666666s" repeatCount="indefinite"></animate>
+  </rect>
+</g><g transform="rotate(300 50 50)">
+  <rect x="45" y="10" rx="4.5" ry="1" width="10" height="40" fill="#a9a9a9">
+    <animate attributeName="opacity" values="1;0" times="0;1" dur="1s" begin="0s" repeatCount="indefinite"></animate>
+  </rect>
+</g></svg>
+</template>
+<script>
+export default {};
+</script>
+<style>
+</style>
