a54dffb0e7db54ead069e62957b20224ada9c392
Charles Zinn <charles.zinn@gmail.com>
2017-10-14 12:05:03 -0400

    Create, join, and leave lobbies.

--- web/src/components/pages/LobbyDetailPage.vue
+++ web/src/components/pages/LobbyDetailPage.vue
@@ -31,18 +31,32 @@
             .bignum.faint(v-if='lobby.players.length === 0')
               | None
         .lobby-controls
-          button(@click='join') Join
-          button(@click='start') Start
+          template(v-if='!lobby.game_id')
+            button(v-if='in_lobby' @click='leave' key='leave') Leave
+            button(v-else @click='join' key='join') Join
+            button(@click='start') Start
+          button(v-else @click='view_game') View Game
 </template>
 <script>
-import api from '@/api';
 import Spinner from '@/components/Spinner';
+import api from '@/api';
 import games from '@/games/index';
+import globals from '@/globals';
 
 export default {
   components: { Spinner },
   computed: {
     lobby_id() { return this.$route.params.id; },
+    current_user() { return globals.current_user; },
+    in_lobby() {
+      if (!this.lobby) {
+        return false;
+      }
+      const rval = this.lobby.players.find(
+        player => player.id === this.current_user.id,
+      ) !== undefined;
+      return rval;
+    },
   },
   data: () => ({
     lobby: undefined,
@@ -61,10 +75,30 @@ export default {
       });
     },
     join() {
-      // TODO
+      api.post(`lobbies/${this.lobby_id}/join`, {}).then((data) => {
+        this.lobby = data;
+      });
+    },
+    leave() {
+      api.post(`lobbies/${this.lobby_id}/leave`, {}).then((data) => {
+        if (!data.deleted) {
+          this.lobby = data;
+        } else {
+          this.$router.push({ name: 'LobbyPage' });
+        }
+      });
     },
     start() {
-      // TODO
+      api.post(`lobbies/${this.lobby_id}/start`, {}).then((data) => {
+        this.lobby = data;
+        if (data.game_id) {
+          this.view_game();
+        }
+      });
+    },
+    view_game() {
+      console.log(this.lobby.game_id);
+      this.$router.push({ name: 'GamePage', params: { id: this.lobby.game_id } });
     },
   },
 };
