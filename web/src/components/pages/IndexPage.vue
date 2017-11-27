8248b3c4e96958c985241ec90993930890e9fe6a
Charles Zinn <charles.zinn@gmail.com>
2017-11-26 20:23:02 -0500

    Show other active games on dashboard

--- web/src/components/pages/IndexPage.vue
+++ web/src/components/pages/IndexPage.vue
@@ -2,10 +2,10 @@
   .main-page
     .main-content(v-if='globals.current_user')
       h1 Dashboard
-      .active-games(v-if='activeGames && activeGames.length > 0')
-        h2 Active games
+      .active-games(v-if='userActiveGames && userActiveGames.length > 0')
+        h2 Your active games
         .lobbies
-          .lobby(v-for='game in activeGames')
+          .lobby(v-for='game in userActiveGames')
             .lobby-row
               .lobby-section
                 .bignum {{ game.name }}
@@ -14,11 +14,22 @@
               router-link(:to="{ name: 'GamePage', params: { id: game.id } }")
                 button(v-if='game.current_turn') Make Move
                 button(v-else) View
-      div(v-else-if='activeGames')
+      div(v-else-if='userActiveGames')
         h2
-          | No active games. 
+          | You have no active games. 
           router-link(:to="{ name: 'LobbyPage' }") Create or join one!
       Spinner(v-else)
+      .other-games(v-if='otherActiveGames && otherActiveGames.length > 0')
+        h2 Other active games
+        .lobbies
+          .lobby(v-for='game in otherActiveGames')
+            .lobby-row
+              .lobby-section
+                .bignum {{ game.name }}
+                h2 {{ game.with }}
+            .lobby-controls
+              router-link(:to="{ name: 'GamePage', params: { id: game.id } }")
+                button View
     .main-content(v-else)
       h1 Welcome to Tableflip — Board Game Server.
       h1 (╯°□°)╯︵ ┻━┻
@@ -55,16 +66,24 @@ export default {
     globals,
   }),
   computed: {
-    activeGames() {
+    userActiveGames() {
       if (globals.userGames) {
         return globals.userGames.filter(game => !game.finished)
           .map(gameView);
       }
       return undefined;
     },
-    finishedGames() {
+    userFinishedGames() {
       if (globals.userGames) {
         return globals.userGames.filter(game => game.finished)
+          .map(gameView);
+      }
+      return undefined;
+    },
+    otherActiveGames() {
+      if (globals.activeGames) {
+        return globals.activeGames.filter(game => !game.players.find(
+            player => player.id === globals.current_user.id))
           .map(gameView);
       }
       return undefined;
@@ -75,10 +94,14 @@ export default {
       globals.poll('indexpage', 'userGames', () => {
         globals.fetchUserGames();
       });
+      globals.poll('indexpage', 'activeGames', () => {
+        globals.fetchActiveGames();
+      });
     },
   },
   destroyed() {
     globals.stopPoll('indexpage', 'userGames');
+    globals.stopPoll('indexpage', 'activeGames');
   },
 };
 </script>
